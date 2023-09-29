23b8c7bc0eead697aa9e0a57cbff12187e6a23d7
darkill <darkill.mc.yt@gmail.com>
2023-09-29 11:46:22 +0200

    amélioration chargement cmd

--- loaders/loadCommands.js
+++ loaders/loadCommands.js
@@ -5,8 +5,9 @@ module.exports = async bot => {
     fs.readdirSync("./commandes").filter(f => f.endsWith(".js")).forEach(async file => {
 
         let command = require(`../Commandes/${file}`)
-        if(!command.name || typeof command.name !== "string") throw new TypeError(`La commande ${file.slice(0, file.length -3)} n'a pas de nom!`)
-        bot.commands.set(command.name, command)
+        console.log(command)
+        if(!command.data.name || typeof command.data.name !== "string") throw new TypeError(`La commande ${file.slice(0, file.length -3)} n'a pas de nom!`)
+        bot.commands.set(command.data.name, command)
         console.log(`[commande] Commande ${file} chargée avec succès!`)
     })
 }
